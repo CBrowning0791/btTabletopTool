a8fdfadb3e3e62cf251d4ebc96e02a259f4d9d95
CBrowning0791 <Cbrowning0791@gmail.com>
2017-06-21 01:09:22 -0400

    Version 1.0 - Added Roll log
    Probably should have just done that, added weapon sheet button for next
    version.

--- btTabletopTool.java
+++ btTabletopTool.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 import	javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class btTabletopTool
 {
@@ -20,29 +21,60 @@ class toolGui extends JFrame implements ActionListener
 {
 	JFrame mainFrame;
 	JButton diceRollBtn;
+	JButton weaponReferenceBtn;
 	JTextField diceRollField;
 	JPanel dicePanel;
+	JPanel listPanel;
+	JPanel topButtonPanel;
 	Random rand = new Random();
 	int diceRoll;
+	JList rollList;
+	DefaultListModel rollListEntries = new DefaultListModel();
 	public toolGui ()
 	{
 		///////////Creating everything///////////
+
 		mainFrame = new JFrame("Battletech Tabletop Tool");
 		diceRollBtn = new JButton("Roll Dice");
-		diceRollField = new JTextField(25);
-		dicePanel = new JPanel(new FlowLayout());
+		weaponReferenceBtn = new JButton("Weapon Sheet");
+		diceRollField = new JTextField(10);
+		dicePanel = new JPanel();
+		dicePanel.setLayout(new BoxLayout(dicePanel, BoxLayout.Y_AXIS));
+		listPanel = new JPanel(new FlowLayout());
+		topButtonPanel = new JPanel(new FlowLayout());
+		rollList = new JList(rollListEntries);
+		JScrollPane listPane = new JScrollPane(rollList);
+
 		///////////adding stuff to various panels///////////
+
 		dicePanel.add(diceRollField);
+		dicePanel.add(Box.createRigidArea(new Dimension(0,5)));
 		dicePanel.add(diceRollBtn);
-		mainFrame.add(dicePanel, BorderLayout.NORTH);
+		listPanel.add(listPane);
+		topButtonPanel.add(weaponReferenceBtn);
+		mainFrame.add(dicePanel, BorderLayout.CENTER);
+		mainFrame.add(listPanel, BorderLayout.LINE_END);
+		mainFrame.add(topButtonPanel, BorderLayout.PAGE_START);
+
+		///////////cleaning up sizing issues///////////
+
+		diceRollField.setMaximumSize(new Dimension(Integer.MAX_VALUE, diceRollField.getPreferredSize().height));
+		dicePanel.setBorder(new EmptyBorder(5,10,10,10));
+
 		///////////making things visible///////////
+
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setSize(500,500);
+		mainFrame.setSize(500,300);
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
+
 		///////////adding actionListeners///////////
+
 		diceRollBtn.setActionCommand("ROLL");
 		diceRollBtn.addActionListener(this);
+		weaponReferenceBtn.setActionCommand("WEAPON");
+		weaponReferenceBtn.addActionListener(this);
+
 	}
 
 	public void actionPerformed(ActionEvent e)
@@ -53,6 +85,11 @@ class toolGui extends JFrame implements ActionListener
 			case "ROLL":    diceRoll = rand.nextInt(6)+ 1;
 							diceRoll += rand.nextInt(6)+ 1;
 							diceRollField.setText(Integer.toString(diceRoll));
+							rollListEntries.add(0, diceRoll);
+							break;
+
+			case "WEAPON":
+
 							break;
 		}
 	}
